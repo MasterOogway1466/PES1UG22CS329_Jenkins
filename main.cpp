11f3dedbccffb41df5504da5dc2105076d58ba18
Manas-Shetty <manas.shetty04@gmail.com>
2025-03-13 16:15:24 +0530

    t5

---
+++ main.cpp
@@ -0,0 +1,13 @@
+// File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "My name is Manas Shetty\n";
+  cout << "My SRN is PES1UG22CS329\n";
+  cout << "I study at PES University\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
