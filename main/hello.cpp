2daeed902b16a7dcfa9e53b486194a222f734b7c
Manas-Shetty <manas.shetty04@gmail.com>
2025-03-13 16:02:12 +0530

    t4 added my name

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "My name is Manas Shetty\n";
   cout << "My SRN is PES1UG22CS329\n";
   cout << "I have successfully built and run this  \n";
   return 0;
