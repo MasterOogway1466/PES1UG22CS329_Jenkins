88d9e444a948807c4521af2fdef98052ce03df21
Manas-Shetty <manas.shetty04@gmail.com>
2025-03-13 15:38:42 +0530

    t2 commits

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,11 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "My SRN is PES1UG22CS329\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
