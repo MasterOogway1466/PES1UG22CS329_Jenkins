118dc81b1ed02054f69c3b02f7812e4e54e03f94
Manas-Shetty <manas.shetty04@gmail.com>
2025-03-13 16:07:58 +0530

    t4 wrote clg name

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,7 @@ int main(void) {
   cout << "Hello, Jenkins\n";
   cout << "My name is Manas Shetty\n";
   cout << "My SRN is PES1UG22CS329\n";
+  cout << "I study at PES University\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
